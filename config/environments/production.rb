1ad613e097e89e49f45af153500288f57ce36b15
Ola Loevholm <ola@lovholm.net>
2012-05-25 15:36:15 +0200

    another try

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,7 @@ EscSys::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
-  config.assets.precompile << ['*.js', '*.css', 'blueprint/print','blueprint/screen']
+  config.assets.precompile << ['*.js', '*.css', 'blueprint/print.css','blueprint/screen.css']
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
