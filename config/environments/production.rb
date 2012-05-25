56943edae19487884d4a50a95d198c45db35869a
Ola Loevholm <ola@lovholm.net>
2012-05-25 15:33:48 +0200

    precompilation error fixed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,7 @@ EscSys::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
-  config.assets.precompile << ['*.js', '*.css']
+  config.assets.precompile << ['*.js', '*.css', 'blueprint/print','blueprint/screen']
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
