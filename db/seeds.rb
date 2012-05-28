754ca4d6ea140f7a449bba78b9f1c0260916d6a5
Ola Loevholm <ola@lovholm.net>
2012-05-28 15:28:00 +0200

    seed data added

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,25 +4,29 @@
 #
 # Examples:
 #
-require 'net/http'
-def find_name(code)
-  req = Net::HTTP.get_response(URI.parse('http://www.foseid.priv.no/gustav/iso3166/iso3166-nb.txt'))
-  @play = req.body
-  @play = @play.force_encoding('UTF-8')
-  if @play =~ /^(#{code})\s([a-å\w\WA-Å]*)$/i
-    puts @play
-  end
-
-end
-
-def find_nor_name
-  
-end
-
-
-
-@countries = ["GB", "HU", "AL", "RU"]
-
-@countries.each do |c|
-  find_name(c)
-end
+Country.create :name => "United Kingdom", :nor_name=> "Storbritannia", :short_id=> "gb", :actor_name=> "Engelbert Humperdinck", :song_name=> "Love Will Set You Free"
+Country.create :name => "Hungary", :nor_name=> "Ungarn", :short_id=> "hu", :actor_name=> "Compact Disco", :song_name=> "Sound Of Our Hearts"
+Country.create :name => "Albania", :nor_name=> "Albania", :short_id=> "al", :actor_name=> "Rona Nishliu", :song_name=> "Suus"
+Country.create :name => "Lithuania", :nor_name=> "Litauen", :short_id=> "lt", :actor_name=> "Donny Montell", :song_name=> "Love Is Blind"
+Country.create :name => "Bosnia & Herzegovina", :nor_name=> "Bosnia-Hercegovina", :short_id=> "ba", :actor_name=> "Maya Sar", :song_name=> "Korake Ti Znam"
+Country.create :name => "Russia", :nor_name=> "Russland", :short_id=> "ru", :actor_name=> "Buranovskiye Babushki", :song_name=> "Party For Everybody"
+Country.create :name => "Iceland", :nor_name=> "Island", :short_id=> "is", :actor_name=> "Greta Salóme & Jónsi", :song_name=> "Never Forget"
+Country.create :name => "Cyprus", :nor_name=> "Kypros", :short_id=> "cy", :actor_name=> "Ivi Adamou", :song_name=> "La La Love"
+Country.create :name => "France", :nor_name=> "Frankrike", :short_id=> "fr", :actor_name=> "Anggun", :song_name=> "Echo (You And I)"
+Country.create :name => "Italy", :nor_name=> "Italia", :short_id=> "it", :actor_name=> "Nina Zilli", :song_name=> "L'Amore È Femmina (Out Of Love)"
+Country.create :name => "Estonia", :nor_name=> "Estland", :short_id=> "ee", :actor_name=> "Ott Lepland", :song_name=> "Kuula"
+Country.create :name => "Norway", :nor_name=> "Norge", :short_id=> "no", :actor_name=> "Tooji", :song_name=> "Stay"
+Country.create :name => "Azerbaijan", :nor_name=> "Aserbajdsjan", :short_id=> "az", :actor_name=> "Sabina Babayeva", :song_name=> "When The Music Dies"
+Country.create :name => "Romania", :nor_name=> "Romania", :short_id=> "ro", :actor_name=> "Mandinga", :song_name=> "Zaleilah"
+Country.create :name => "Denmark", :nor_name=> "Danmark", :short_id=> "dk", :actor_name=> "Soluna Samay", :song_name=> "Should've Known Better"
+Country.create :name => "Greece", :nor_name=> "Hellas", :short_id=> "gr", :actor_name=> "Eleftheria Eleftheriou", :song_name=> "Aphrodisiac"
+Country.create :name => "Sweden", :nor_name=> "Sverige", :short_id=> "se", :actor_name=> "Loreen", :song_name=> "Euphoria"
+Country.create :name => "Turkey", :nor_name=> "Tyrkia", :short_id=> "tr", :actor_name=> "Can Bonomo", :song_name=> "Love Me Back"
+Country.create :name => "Spain", :nor_name=> "Spania", :short_id=> "es", :actor_name=> "Pastora Soler", :song_name=> "Quédate Conmigo (Stay With Me)"
+Country.create :name => "Germany", :nor_name=> "Tyskland", :short_id=> "de", :actor_name=> "Roman Lob", :song_name=> "Standing Still"
+Country.create :name => "Malta", :nor_name=> "Malta", :short_id=> "mt", :actor_name=> "Kurt Calleja", :song_name=> "This Is The Night"
+Country.create :name => "F.Y.R. Macedonia", :nor_name=> "Makedonia", :short_id=> "mk", :actor_name=> "Kaliopi", :song_name=> "Crno I Belo"
+Country.create :name => "Ireland", :nor_name=> "Irland", :short_id=> "ie", :actor_name=> "Jedward", :song_name=> "Waterline"
+Country.create :name => "Serbia", :nor_name=> "Serbia", :short_id=> "rs", :actor_name=> "Željko Joksimović", :song_name=> "Nije Ljubav Stvar"
+Country.create :name => "Ukraine", :nor_name=> "Ukraina", :short_id=> "ua", :actor_name=> "Gaitana", :song_name=> "Be My Guest"
+Country.create :name => "Moldova", :nor_name=> "Moldova", :short_id=> "md", :actor_name=> "Pasha Parfeny", :song_name=> "Lăutar"
